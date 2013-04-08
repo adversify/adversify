1b36080e6ded56c249b36724f44b5d4fb9e52bd4
Timothée PILLARD <pillard@intechinfo.fr>
2013-04-08 10:03:45 +0200

    Refactored Website Manager Module, it was horrible

--- modules/website-manager.js
+++ modules/website-manager.js
@@ -1,175 +1,160 @@
-/* This module is so not ready for production!*/
-/* e -> error
-/* o -> object
+var mongoose = require('mongoose'),
+ZM = require('../modules/zone-manager.js'),
+WM = {};
 
+module.exports = WM; // WEBSITE MANAGER MODULE
+
+/*
+ * Retrieve publishers websites list
+ * @function
+ * @param {String} uId userId from session
+ * @param {String} nb number of results to fetch
+ * @param {String} sort sort criteria, 'ascending' or 'descending'
+ * @param {Function} callback(err, result) function that is used to give back results or error
 */
-var mongoose = require('mongoose');
-var ZM = require('../modules/zone-manager.js');
-
-var WM = {};
-
-module.exports = WM;
 
 
-WM.getWebsites = function(u,nb,sort,callback) { // Publisher username, number of websites to display, sort criteria and callback
-	var urls = [];
-	var tempW;
-	PublisherModel.findOne({username:u}, function(e,o) {
-		if(o) {
-			tempW = o.websites;
-			for(var i=0;i < tempW.length; i++) {
-				urls.push(tempW[i].url);
-			}
-			console.log(urls);
-			WebsiteModel.find({url:{$in: urls}}, function(e,o) {
+WM.getWebsites = function(uId,nb,sort,callback) {
+	var websiteIds = [];
+
+	PublisherModel.findOne({_id:uId}, function(e,publisher) {
+		if(!publisher || e) {
+			websiteIds = publishers.websites;
+			WebsiteModel.find({_id:{$in: websiteIds}}, function(e, fetchedWebsites) {
+				if(e || !fetchedWebsites || !fetchedWebsites.length) {
+					callback(e);
+				} else {
+					callback(null, fetchedWebsites);
+				}
+			});
+
+		} else {
+			callback(e);
+		}
+	});
+};
+
+/*
+ * Update a single website
+ * @function
+ * @param {String} uId userId from session
+ * @param {String} websiteId _id of the targeted website
+ * @param [{String}] newData website data to be updated via POST or PUT request 
+ * @param {Function} callback(err,result) function that is used to give back results or error
+*/
+
+WM.updateWebsite = function(uId, websiteId, newData, callback) {
+	PublisherModel.findOne({_id:uId}, function(e, publisher) {
+		if(e || !publisher) {
+			callback(e);
+		} else {
+			WebsiteModel.findOne({_id:websiteId}, function(e, updatedWebsite) {
+				if(e || !updatedWebsite) {
+					callback(e);
+				} else {
+					callback(null, updatedWebsite);
+				}
+			});
+		}
+	});
+};
+
+/*
+ * Retrieve single website
+ * @function
+ * @param {String} uId userId from session
+ * @param {String} wId websiteId of the website
+ * @param {Function} callback function that is used to give back results
+*/
+
+WM.getWebsite = function(uId, wId, callback) {
+	PublisherModel.findOne({_id: uId, websites: { '$in' : [wId] }}, function(e, publisher) {
+		if(e || !publisher) {
+			callback(e);
+		} else {
+			WebsiteModel.findOne({_id:wId}, function(e,o) {
+				if(o) {
+					callback(null,o);
+				} else {
+					callback(e);
+				}
+			});
+		}
+	});
+};
+
+
+/*
+ * Deletes single website
+ * @function
+ * @param {String} uId userId from session
+ * @param {String} wId websiteId of the website
+ * @param {Function} callback(err, result) function that is used to give back results
+*/
+
+WM.deleteWebsite = function(uId, wId, callback) {
+
+	ZM.deleteZonesByWebsite(uId, nId, function(e, deletedZones) {
+		if(e || !deletedZones) {
+			callback(e);
+		} else {
+			WebsiteModel.remove({_id: wId}, function(e) {
 				if(e) {
 					callback(e);
 				} else {
-					callback(null,o);
+					PublisherModel.findOne({_id: uId}, function(e, publisher) {
+						if(e || !publisher) {
+							callback(e);
+						} else {
+							publisher.websites.pop(wId);
+							publisher.save();
+						}
+					});
 				}
 			});
-
-		} else {
-			callback(e);
 		}
 	});
-}
-// TO DO
 
-WM.updateWebsite = function(u, w, newData, callback) { // Publisher username, website ID, callback // TO DO
-	var user;
-	PublisherModel.findOne({username:u}, function(e,o) {
-		if(o) {
-			user = o;
-			WebsiteModel.findOne({_id:w._id}, function(e,o) {
-				if(!e) {
-					w = o;
-				} else {
+};
+
+/*
+ * Create a website
+ * @function
+ * @param {String} uId userId from session
+ * @param [{String}] newData : POST data to create a website
+ * @param {Function} callback(err, result) function that is used to give back results
+*/
+
+WM.addWebsite = function(uId,newData,callback) {
+
+	WebsiteModel.findOne({"url":newData.url}, function(e, websiteThatAlreadyExists) {
+		if(e || !websiteAlreadyExists) {
+			callback(e);
+		} else {
+			var newWebsite = new WebsiteModel({
+				'name': newData.name,
+				'category': newData.category,
+				'description':newData.description,
+				'url':newData.url,
+				'created':Date.now()
+			});
+			newWebsite.save(function(e,savedWebsite) {
+				if(e || !savedWebsite) {
 					callback(e);
+				} else {
+					PublisherModel.findOne({'_id': uId}, function(e, publisher) {
+						publisher.websites.push(savedWebsite._id);
+						publisher.save(function(e) {
+							if(e) {
+								callback(e);
+							} else {
+								callback(null, savedWebsite);
+							}
+						});
+					});
 				}
 			});
-		} else {
-			callback(e);
-		}
-	})
-}
-
-
-
-WM.getWebsite = function(webSite, callback) { // Publisher username, website ID, callback // TO DO
-	WebsiteModel.findOne({url:webSite}, function(e,o) {
-		if(o) {
-			callback(null,o);
-		} else {
-			callback(e);
 		}
 	});
-}
-
-
-WM.deleteWebsite = function(uId,nId,callback) { // to do ; w._id ???
-	console.log("delete website?");
-
-	ZM.deleteZonesByWebsite(uId, nId, function(e,o) {
-		console.log("ZM.deleteZonesByWebsite"+o);
-		if(e) {
-			callback(e);
-		} else if(o == "OK"){
-			console.log("Zones successfully removed");
-			PublisherModel.findOne({_id:uId, "websites._id":nId},
-			function(e,o) {
-			 	if(e) {
-			 		callback(e);
-			 	} else if(o){
-			 		o.websites.id(nId).remove();
-			 		o.save(function (e,o) {
-						WebsiteModel.remove({_id:nId}, function(e) {
-					 		if(e) {
-					 			callback(e);
-					 		} else {
-					 			callback(null,"OK");
-					 			var action = new ActionModel({
-					 				name:"add-website",
-					 				userId:uId,
-					 				itemId: nId
-					 			});
-					 			action.save();
-					 		}
-						});
-			 		});
-			 	} else {
-			 		callback("Could not find website for this publisher");
-			 	}
-			});
-		}
-	});
-}
-
-
-WM.addWebsite = function(u,newData,callback) { // Publisher username, new websites data in json and callback
-		console.log(newData);
-
-		PublisherModel.findOne({username:u,"websites.url":newData.url}, function(e,o) {
-			if(o && !e) {
-				console.log("update-zones");
-				WebsiteModel.findOne({"url":newData.url}, function(e,o) {
-					if(o) {
-						o.zones = newData.zones;
-						o.save(function(e,o) {
-							if(o) {
-								callback(null,"OK");
-							} else if(e){
-								callback(e);
-							} else allback("Unexpected event occured");
-						});
-					} else if(e) {
-						callback(e)
-					} else callback("Unexpected event occured");
-				});
-			}
-			else if(!o && !e) {
-				var w = new WebsiteModel({
-			"name":newData.name,
-			"category":newData.category,
-			"description":newData.description,
-			"url":newData.url,
-			"created":Date.now()
-		});
-		w.save(function(e,o){
-			if(!e) {
-				PublisherModel.findOneAndUpdate(
-				  { username:u },
-				  { $push: { websites: 
-				  	{
-						"name":newData.name,
-						"category":newData.category,
-						"description":newData.description,
-						"url":newData.url,
-						"created":Date.now(),
-						"_id":o._id				  	
-				  	} 
-				}},
-				  { safe: true, upsert: true },
-				  function(e, o) {
-				    if(e) {
-				    	console.log(" NO NO NO 222");
-				    	callback(e);
-				    } else {
-				    	callback(null,o);
-				    }
-				});	
-			} else {
-				callback(e);
-			}
-		});
-			} else if(e){
-				callback(e);
-			} else {
-				callback("Something weird happenned");
-			}
-		})
-		
-
-
-			//http://stackoverflow.com/questions/13412579/node-express-mongoose-sub-collection-document-insert?rq=1
-}
+};
+//http://stackoverflow.com/questions/13412579/node-express-mongoose-sub-collection-document-insert?rq=1
