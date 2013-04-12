f9612b3c43544d1cb19510439782789607c941df
Timothée PILLARD <pillard@intechinfo.fr>
2013-04-12 19:55:18 +0200

    Finally took a decision about the structure off the data : children need to know who their parents are, but the parents do not need to know who their children are

--- schemas/Publishers.js
+++ schemas/Publishers.js
@@ -7,16 +7,16 @@ var Publisher = new Schema({
     username: { type: String, required: true, match: /^[a-zA-Z0-9-_]+$/, unique: true },
     password: { type: String, required: true},
     salt: { type: String },
-    email: { type: String, unique: true, match : /[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/, required: true },
     modified: { type: Date, default: Date.now },
     joined: {type: Date},
-    streetadress: { type: String },
-    city: { type: String },
-    country: { type: String },
-    phone: { type: String},
     balance: { type: Number, default:0 },
-    websites: [{type: String}],
-    zones: [{type:String}]
+    infos : {
+        streetadress: { type: String },
+        city: { type: String },
+        country: { type: String },
+        phone: { type: String},
+        email: { type: String, unique: true, match : /[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/, required: true }
+    }
 });
 
 
--- schemas/Websites.js
+++ schemas/Websites.js
@@ -3,16 +3,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Website = new Schema({
-    name: { type: String, required: true }, 
-    url : { type : String, match : /((http:\/\/|https:\/\/)?(www.)?(([a-zA-Z0-9-]){2,}\.){1,4}([a-zA-Z]){2,6}(\/([a-zA-Z-_\/\.0-9#:?=&;,]*)?)?)/, required: true, unique: true },
-    description : { type : String },
-    category : { type : String }, 
-    validated : { type : Boolean, default: false },
     modified: { type: Date, default: Date.now },
     created: {type: Date},
-    validatedOn: {type: Date},
-    zones: [{ type: String }],
+    validation: {
+        validated : { type : Boolean, default: false },
+        validatedOn: {type: Date}
+    },
     owner: {type: String},
+    infos: {
+        name: { type: String, required: true },
+        url : { type : String, match : /((http:\/\/|https:\/\/)?(www.)?(([a-zA-Z0-9-]){2,}\.){1,4}([a-zA-Z]){2,6}(\/([a-zA-Z-_\/\.0-9#:?=&;,]*)?)?)/, required: true, unique: true },
+        description : { type : String },
+        category : { type : String },
+        tags: [{type: String}]
+    }
 });
 
 WebsiteModel = mongoose.model('websites', Website);
